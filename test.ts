cae4173dd0191c6b0673bbd10e5e8d99f21daf97
axetroy <troy450409405@gmail.com>
2019-05-13 11:36:27 +0800

    chore: add travis

--- test.ts
+++ test.ts
@@ -1,5 +1,9 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
+import { runIfMain } from "https://deno.land/std/testing/mod.ts";
+
 import "./big/test.ts";
 import "./min_test.ts";
 import "./max_test.ts";
 import "./sum_test.ts";
+
+runIfMain(import.meta);
