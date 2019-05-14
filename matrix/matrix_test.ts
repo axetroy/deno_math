a8abd54f18d137b73ee68ba7821e9860785e0ce5
axetroy <troy450409405@gmail.com>
2019-05-14 14:24:28 +0800

    add testcase

--- matrix/matrix_test.ts
+++ matrix/matrix_test.ts
@@ -186,6 +186,25 @@ test({
         [2, 4, 6]
       ]
     );
+
+    assertEquals(
+      new Matrix(
+        // prettier-ignore
+        [
+          [1, 2],
+          [3, 4],
+          [5, 6]
+        ]
+      )
+        .transpose()
+        .transpose().matrix,
+      [
+        // prettier-ignore
+        [1, 2],
+        [3, 4],
+        [5, 6]
+      ]
+    );
   }
 });
 
